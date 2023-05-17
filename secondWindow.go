2bcd6dd4097f5625ec056f6285e4e41c55285d69
Ewald Horn <ewaldhorn@gmail.com>
2023-05-17 15:42:06 +0200

    Better layout on SecondWindow for presentation

--- secondWindow.go
+++ secondWindow.go
@@ -8,13 +8,17 @@ import (
 
 func createSecondWindow(application fyne.App) {
 	secondWindow := application.NewWindow("This is distracting")
+
 	secondWindow.SetContent(widget.NewProgressBarInfinite())
+
 	secondWindow.Resize(fyne.Size{
 		Width:  250,
 		Height: 20,
 	})
+
 	secondWindow.SetCloseIntercept(func() {
 		fmt.Println("LOL! Nice try! Not happening!")
 	})
+
 	secondWindow.Show()
 }
